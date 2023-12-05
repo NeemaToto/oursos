0ceef08ab25a408b31f20f9d8d21d08effe71114
Neema Totonchi <neematotonchi@Neemas-MacBook-Pro.local>
2023-12-05 02:56:12 -0800

    added feedback to user submitting a message

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,16 +1,20 @@
 import { TextInput, Textarea, SimpleGrid, Group, Title, Button, Card, Container, useMantineTheme, rem } from '@mantine/core';
+import { useTimeout } from '@mantine/hooks';
 import classes from './Contact.module.css';
 import { useState } from 'react'
 import { IconAt } from '@tabler/icons-react';
 
 export function Contact() {
+  const theme = useMantineTheme();
+  const { start } = useTimeout(() => handleButtonChange(), 2000);
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [subject, setSubject] = useState('')
   const [message, setMessage] = useState('')
+  const [buttonColor, setButtonColor] = useState(theme.colors.gold[6])
+  const [buttonMessage, setButtonMessage] = useState('Send Message')
 
   const icon = <IconAt style={{ width: rem(16), height: rem(16) }} />;
-  const theme = useMantineTheme();
 
   const data = {
     name: name,
@@ -22,7 +26,6 @@ export function Contact() {
   const url = 'https://script.google.com/macros/s/AKfycbylN58nSIdAOw2PvJRmUdTMZNyXdHbnz3XianG-2DYHe8VaSJUrJw5qGtKM2axlM7dF/exec'
 
   function handleSubmit() {
-
     if (name.length > 2 && email.length > 2 && message.length > 2) {
       fetch(url, {
         method: 'POST',
@@ -34,17 +37,25 @@ export function Contact() {
         .then((res) => res.json())
         .then((data) => console.log('data', data))
         .catch((err) => console.log('err', err));
+      setName('')
+      setEmail('')
+      setSubject('')
+      setMessage('')
+      setButtonColor('green')
+      setButtonMessage('Sent!')
+      start()
     } else {
-      console.log('Make sure you\'ve correctly filled out each input')
     }
+  }
 
-    console.log('this function has ran')
+  function handleButtonChange() {
+    setButtonColor(theme.colors.gold[6])
+    setButtonMessage('Send Message')
   }
 
   return (
     <Container style={{ marginTop: '3rem' }} size="lg" py="xl">
       <Card shadow="md" radius="md" className={classes.card} padding="xl" py='xl'>
-
         <Title
           order={2}
           size="h1"
@@ -103,8 +114,8 @@ export function Contact() {
         />
 
         <Group justify="center" mt="xl">
-          <Button type="submit" size="md" color={theme.colors.gold[6]} onClick={() => handleSubmit()}>
-            Send message
+          <Button type="submit" size="md" color={buttonColor} onClick={() => handleSubmit()}>
+            {buttonMessage}
           </Button>
         </Group>
       </Card>
